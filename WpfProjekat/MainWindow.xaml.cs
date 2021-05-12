015b68a09ed7bcbc38d4d2e1689110efd9af0d62
Vasilije99 <vasilije240799@gmail.com>
2021-05-12 14:13:11 +0200

    aa

---
+++ WpfProjekat/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfProjekat.Klase;
+
+namespace WpfProjekat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBoxDrzave();
+        }
+
+        private void ComboBoxDrzave()
+        {
+            List<string> drzave = new List<string>();
+            drzave.Add("srbija");
+            drzave.Add("hrvatska");
+            comboBoxDrzave.ItemsSource = drzave;
+        }
+
+        private async void ButtonPotrosnja_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a file";
+            op.Filter = "All files (*.*)|*.*";
+            
+            if(op.ShowDialog() == true)
+            {
+                string fileName = op.FileName;
+                string path = System.IO.Path.GetFullPath(op.FileName);
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                DataSet ds = new DataSet();
+                Microsoft.Office.Interop.Excel.Workbook wb = excel.Workbooks.Open(path);
+                foreach(Microsoft.Office.Interop.Excel.Worksheet ws in wb.Worksheets)
+                {
+                    System.Data.DataTable td = new System.Data.DataTable();
+                    td = await Task.Run(() => formofDataTable(ws));
+                    ds.Tables.Add(td);
+                }
+                tabela.ItemsSource = ds.Tables[0].DefaultView;
+                
+                wb.Close();
+            }
+        }
+
+        private System.Data.DataTable formofDataTable(Worksheet ws)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+            string worksheetName = ws.Name;
+            dt.TableName = worksheetName;
+            Microsoft.Office.Interop.Excel.Range xlRange = ws.UsedRange;
+            object[,] valueArray = (object[,])xlRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+            for (int k = 1; k <= valueArray.GetLength(1); k++)
+            {
+                dt.Columns.Add((string)valueArray[1, k]);  //add columns to the data table.
+            }
+            object[] singleDValue = new object[valueArray.GetLength(1)]; //value array first row contains column names. so loop starts from 2 instead of 1
+            for (int i = 2; i <= valueArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < valueArray.GetLength(1); j++)
+                {
+                    if (valueArray[i, j + 1] != null)
+                    {
+                        singleDValue[j] = valueArray[i, j + 1].ToString();
+                    }
+                    else
+                    {
+                        singleDValue[j] = valueArray[i, j + 1];
+                    }
+                }
+                dt.LoadDataRow(singleDValue, System.Data.LoadOption.PreserveChanges);
+            }
+
+            return dt;
+        }
+    }
+}
