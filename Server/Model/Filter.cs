4e993587f50a34ed130dfc6dc3b5384ca66b1e2a
Vasilije99 <80057954+Vasilije99@users.noreply.github.com>
2021-05-20 16:52:53 +0200

    d

---
+++ Server/Model/Filter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Model
+{
+    public class Filter : ValidationBase
+    {
+        private string drzavaFilter { get; set; }
+        private DateTime pocetniDatumFilter { get; set; }
+        private DateTime krajnjiDatumFilter { get; set; }
+
+        public string DrzavaFilter
+        {
+            get { return drzavaFilter; }
+            set
+            {
+                if (drzavaFilter != value)
+                {
+                    drzavaFilter = value;
+                    OnPropertyChanged("DrzavaFilter");
+                }
+            }
+        }
+
+        public DateTime PocetniDatumFilter
+        {
+            get { return pocetniDatumFilter; }
+            set
+            {
+                if (pocetniDatumFilter != value)
+                {
+                    pocetniDatumFilter = value;
+                    OnPropertyChanged("PocetniDatumFilter");
+                }
+            }
+        }
+
+        public DateTime KrajnjiDatumFilter
+        {
+            get { return krajnjiDatumFilter; }
+            set
+            {
+                if (krajnjiDatumFilter != value)
+                {
+                    krajnjiDatumFilter = value;
+                    OnPropertyChanged("KrajnjiDatumFilter");
+                }
+            }
+        }
+
+        protected override void ValidateSelf()
+        {
+            if (string.IsNullOrWhiteSpace(this.drzavaFilter))
+            {
+                this.ValidationErrors["DrzavaFilter"] = "Nije izabrana drzava!";
+            }
+            if (this.pocetniDatumFilter == null)
+            {
+                this.ValidationErrors["PocetniDatumFilter"] = "Pocetni datum nije izabran!";
+            }
+            if (this.krajnjiDatumFilter == null)
+            {
+                this.ValidationErrors["KrajnjiDatumFilter"] = "Krajnji datum nije izabran!";
+            }
+        }
+    }
+}
