748366a25adac5b2849aa9d93ee8cb72b5b5aeaa
Bojan Brdarevic <bojanbrdarevic@gmail.com>
2021-06-02 13:11:56 +0200

    commit

---
+++ EvidencijaPotrosnjeIVremenskihParametara/Klase/Drzava.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencijaPotrosnjeIVremenskihParametara.Klase
+{
+    public class Drzava
+    {
+        string nazivDrzave;
+        string sifraDrzave;
+        string nazivMernogMesta;
+
+        public static List<Drzava> drzave = new List<Drzava>();
+        
+        public string NazivDrzave { get => nazivDrzave; set => nazivDrzave = value; }
+        public string SifraDrzave { get => sifraDrzave; set => sifraDrzave = value; }
+        public string NazivMernogMesta { get => nazivMernogMesta; set => nazivMernogMesta = value; }
+
+        public Drzava()
+        {
+            NazivDrzave = "";
+            SifraDrzave = "";
+            NazivMernogMesta = "";
+        }
+
+        public Drzava(string nazivDrzave, string sifraDrzave, string nazivMernogMesta)
+        {
+            this.NazivDrzave = nazivDrzave;
+            this.SifraDrzave = sifraDrzave;
+            this.NazivMernogMesta = nazivMernogMesta;
+        }
+       
+    }
+}
---
+++ EvidencijaPotrosnjeIVremenskihParametara/Klase/VremenskiPodaci.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencijaPotrosnjeIVremenskihParametara.Klase
+{
+    public class VremenskiPodaci
+    {
+        string mernoMesto;
+        double temperatura;
+        double atmPritisak;
+        double vlaznostVazduha;
+        double brzinaVetra;
+        DateTime datum;
+        
+
+        public double Temperatura { get => temperatura; set => temperatura = value; }
+        public double AtmPritisak { get => atmPritisak; set => atmPritisak = value; }
+        public double VlaznostVazduha { get => vlaznostVazduha; set => vlaznostVazduha = value; }
+        public double BrzinaVetra { get => brzinaVetra; set => brzinaVetra = value; }
+        public string MernoMesto { get => mernoMesto; set => mernoMesto = value; }
+        public DateTime Datum { get => datum; set => datum = value; }
+
+        public VremenskiPodaci()
+        {
+            MernoMesto = "";
+            Temperatura = 0;
+            AtmPritisak = 0;
+            VlaznostVazduha = 0;
+            BrzinaVetra = 0;
+            Datum = DateTime.Now;
+        }
+
+        public VremenskiPodaci(string mernoMesto, double temperatura, double atmPritisak, double vlaznostVazduha, double brzinaVetra, DateTime datum)
+        {
+            MernoMesto = mernoMesto;
+            Temperatura = temperatura;
+            AtmPritisak = atmPritisak;
+            VlaznostVazduha = vlaznostVazduha;
+            BrzinaVetra = brzinaVetra;
+            Datum = datum;
+        }
+    }
+}
