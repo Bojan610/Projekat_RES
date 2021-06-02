47911a66657416ac36e2337f4b831e7e112c7726
Bojan Brdarevic <bojanbrdarevic@gmail.com>
2021-06-02 12:45:47 +0200

    commit

---
+++ Common/IOperacije.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IOperacije
+    {
+        void Import();
+
+        void Filter();
+
+        void Export();
+
+    }
+}
