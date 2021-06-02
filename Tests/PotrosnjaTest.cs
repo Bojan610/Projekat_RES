1d195860161b01f7e8d48114bd6ea6f147934768
Vasilije99 <80057954+Vasilije99@users.noreply.github.com>
2021-06-02 13:32:51 +0200

    a

---
+++ Tests/PotrosnjaTest.cs
@@ -0,0 +1,51 @@
+﻿using EvidencijaPotrosnjeIVremenskihParametara.Klase;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PotrosnjaTest
+    {
+        [Test]
+        [TestCase("Srbija", "SE", null, 1234)]
+        [TestCase("Nemacka", "DE", null, 3344)]
+        [TestCase("Spanija", "SP", null, 1000)]
+        public void PotrosnjaDobriParametri(string nazivDrzave, string sifraDrzave, DateTime uctVreme,double kolicinaEnergije)
+        {
+            Potrosnja potrosnja = new Potrosnja(nazivDrzave, sifraDrzave, uctVreme, kolicinaEnergije);
+
+            Assert.AreEqual(potrosnja.NazivDrzave, nazivDrzave);
+            Assert.AreEqual(potrosnja.SifraDrzave, sifraDrzave);
+            Assert.AreEqual(potrosnja.KolicinaEnergije, kolicinaEnergije);
+        }
+
+        [Test]
+        [TestCase("", "SE", null, 1234)]
+        [TestCase("Nemacka", "", null, 3344)]
+        [TestCase("Spanija", "SP", null, -1)]
+        [TestCase("srbija", "SE", null, 1000)]
+        [TestCase("Srbija", "sE", null, 1000)]
+        [TestCase("Svajcarska", "Sw", null, 2350)]
+        [TestCase("Rumunija", "ro", null, 1300)]
+        [TestCase("Francuska", "FRA", null, 123)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PotrosnjaLosiParametri(string nazivDrzave, string sifraDrzave, DateTime uctVreme, double kolicinaEnergije)
+        {
+            Potrosnja potrosnja = new Potrosnja(nazivDrzave, sifraDrzave, uctVreme, kolicinaEnergije);
+        }
+
+        [Test]
+        [TestCase(null, "SE", null, 0)]
+        [TestCase("Srbija", null, null, 0)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PotrosnjaNullParametri(string nazivDrzave, string sifraDrzave, DateTime uctVreme, double kolicinaEnergije)
+        {
+            Potrosnja potrosnja = new Potrosnja(nazivDrzave, sifraDrzave, uctVreme, kolicinaEnergije);
+        }
+    }
+}
---
+++ Tests/VremenskiPodaciTest.cs
@@ -0,0 +1,52 @@
+﻿using EvidencijaPotrosnjeIVremenskihParametara.Klase;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class VremenskiPodaciTest
+    {
+        [Test]
+        [TestCase("Beograd", 1, 12, 3, 2, null)]
+        [TestCase("Tirana", 31, 2, 5, 1.9, null)]
+        [TestCase("Rim", 21, 1, 4, 3, null)]
+        public void VremenskiPodaciDobriParametri(string mernoMesto, double temperatura, double atmPritisak, double vlaznostVazduha, double brzinaVetra, DateTime datum)
+        {
+            VremenskiPodaci vremenskiPodaci = new VremenskiPodaci(mernoMesto, temperatura, atmPritisak, vlaznostVazduha, brzinaVetra, datum);
+
+            Assert.AreEqual(vremenskiPodaci.MernoMesto, mernoMesto);
+            Assert.AreEqual(vremenskiPodaci.Temperatura, temperatura);
+            Assert.AreEqual(vremenskiPodaci.AtmPritisak, atmPritisak);
+            Assert.AreEqual(vremenskiPodaci.VlaznostVazduha, vlaznostVazduha);
+            Assert.AreEqual(vremenskiPodaci.BrzinaVetra, brzinaVetra);
+            Assert.AreEqual(vremenskiPodaci.Datum, datum);
+        }
+
+        [Test]
+        [TestCase("", 1, 12, 3, 2, null)]
+        [TestCase("Tirana", 31, -2, 5, 1.9, null)]
+        [TestCase("Rim", 21, 1, -4, 3, null)]
+        [TestCase("Stokholm", -5, 4, 2, -3, null)]
+        [TestCase("stokholm", -5, 4, 2, -3, null)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void VremenskiPodaciLosiParametri(string mernoMesto, double temperatura, double atmPritisak, double vlaznostVazduha, double brzinaVetra, DateTime datum)
+        {
+            VremenskiPodaci vremenskiPodaci = new VremenskiPodaci(mernoMesto, temperatura, atmPritisak, vlaznostVazduha, brzinaVetra, datum);
+        }
+
+        [Test]
+        [TestCase(null, 1, 12, 3, 2, null)]
+        [TestCase(null, 31, 2, 5, 1.9, null)]
+        [TestCase(null, 21, 1, 4, 3, null)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void VremenskiPodaciNullParametri(string mernoMesto, double temperatura, double atmPritisak, double vlaznostVazduha, double brzinaVetra, DateTime datum)
+        {
+            VremenskiPodaci vremenskiPodaci = new VremenskiPodaci(mernoMesto, temperatura, atmPritisak, vlaznostVazduha, brzinaVetra, datum);
+        }
+    }
+}
