5d935c8393933f19b1b3812d60c286940394da2b
Bojan Brdarevic <bojanbrdarevic@gmail.com>
2021-06-02 14:37:30 +0200

    commit

---
+++ EvidencijaPotrosnjeIVremenskihParametara/ImportWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Win32;
+using Server;
+using Server.Klase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EvidencijaPotrosnjeIVremenskihParametara
+{
+    /// <summary>
+    /// Interaction logic for ImportWindow.xaml
+    /// </summary>
+    public partial class ImportWindow : Window
+    {
+        public ImportWindow()
+        {
+            InitializeComponent();
+
+            ComboBoxDrzaveImport();
+
+            DataContext = this;
+        }
+
+        public bool Validacija()
+        {
+            bool rez = true;
+
+            if (comboBoxDrzave.SelectedItem == null)
+            {
+                rez = false;
+                borderDrzava.BorderBrush = Brushes.Red;
+                labelaGreskaDrzava.Content = "Nije izabrana drzava!";
+            }
+            else
+            {
+                borderDrzava.BorderBrush = Brushes.Green;
+                labelaGreskaDrzava.Content = "";
+            }
+
+            if (datePocetni.SelectedDate == null)
+            {
+                rez = false;
+                datePocetni.BorderBrush = Brushes.Red;
+                labelaGreskaPocetniDatum.Content = "Pocetni datum nije izabran!";
+            }
+            else
+            {
+                datePocetni.BorderBrush = Brushes.Green;
+                labelaGreskaPocetniDatum.Content = "";
+            }
+
+            if (dateKrajnji.SelectedDate == null)
+            {
+                rez = false;
+                dateKrajnji.BorderBrush = Brushes.Red;
+                labelaGreskaKrajnjiDatum.Content = "Krajnji datum nije izabran!";
+            }
+            else
+            {
+                dateKrajnji.BorderBrush = Brushes.Green;
+                labelaGreskaKrajnjiDatum.Content = "";
+            }
+
+            if (labelaPotvrdaPotrosnja.Content.ToString() == "-")
+            {
+                rez = false;
+                labelaGreskaPotrosnja.Content = "Ucitati file o potrosnji!";
+            }
+            else
+            {
+                buttonPotrosnja.BorderBrush = Brushes.Green;
+                labelaGreskaPotrosnja.Content = "";
+            }
+
+            if (labelaPotvrdaVreme.Content.ToString() == "-")
+            {
+                rez = false;
+                labelaGreskaVreme.Content = "Ucitati file o vremenu!";
+            }
+            else
+            {
+                buttonVreme.BorderBrush = Brushes.Green;
+                labelaGreskaVreme.Content = "";
+            }
+
+            return rez;
+        }
+        
+        private void ComboBoxDrzaveImport()
+        {
+            Drzava.drzave = new List<Drzava>();
+            Drzava.drzave.Add(new Drzava("Srbija", "SE", "Beograd"));
+            Drzava.drzave.Add(new Drzava("Albanija", "AL", "Tirana"));
+            Drzava.drzave.Add(new Drzava("Nemacka", "DE", "Berlin"));
+            Drzava.drzave.Add(new Drzava("Italija", "IT", "Rim"));
+            Drzava.drzave.Add(new Drzava("Litvanija", "LT", "Vilnus"));
+            Drzava.drzave.Add(new Drzava("Poljska", "PL", "Lodj"));
+            Drzava.drzave.Add(new Drzava("Norveska", "NO", "Oslo"));
+            Drzava.drzave.Add(new Drzava("Hrvatska", "HR", "Zagreb"));
+            Drzava.drzave.Add(new Drzava("Holandija", "NL", "Amsterdam"));
+            Drzava.drzave.Add(new Drzava("Rumunija", "RO", "Temisvar"));
+
+            List<string> nazivi = new List<string>();
+            foreach(var item in Drzava.drzave)
+            {
+                nazivi.Add(item.NazivDrzave);
+            }
+            comboBoxDrzave.ItemsSource = nazivi;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Validacija())
+            {
+                List<Potrosnja> potrosnja = new List<Potrosnja>();
+
+                foreach(var item in Data.UcitavanjePotrosnje(labelaPotvrdaPotrosnja.Content.ToString()))
+                {
+                    if (comboBoxDrzave.SelectedItem.ToString() == item.NazivDrzave && item.UctVreme >= datePocetni.SelectedDate && item.UctVreme <= dateKrajnji.SelectedDate)
+                    {
+                        potrosnja.Add(item);
+                    }
+                }
+
+                List<Drzava> listDrzave = new List<Drzava>();
+              
+                foreach (Drzava drzava in Drzava.drzave)
+                {
+                    if (comboBoxDrzave.SelectedItem.ToString() == drzava.NazivDrzave)
+                    {
+                        listDrzave.Add(drzava);
+                        break;
+                    }
+                }
+                
+
+                List<VremenskiPodaci> vremenskiPodaci = new List<VremenskiPodaci>();
+
+                foreach (var item in Data.UcitavanjeVremenskihPodataka(labelaPotvrdaVreme.Content.ToString()))
+                {
+                    if(item.Datum >= datePocetni.SelectedDate && item.Datum <= dateKrajnji.SelectedDate)
+                    {
+                        vremenskiPodaci.Add(item);
+                    }
+                }
+                
+                Data.CuvajPodatke(potrosnja, vremenskiPodaci);
+
+                foreach(var item in Data.UcitavanjeTabele())
+                {
+                    if(comboBoxDrzave.SelectedItem.ToString() == item.NazivDrzave)
+                    {
+                        MainWindow.podaci.Add(item);
+                    }
+                }
+
+                BazaPodataka.BazaPodataka.UpisUBazu(potrosnja, vremenskiPodaci, listDrzave);
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Polja nisu dobro popunjena!", "Greska!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ButtonVreme_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a file";
+            op.Filter = "All files (*.*)|*.*";
+
+            if (op.ShowDialog() == true)
+            {
+                string fileName = op.FileName;
+                //string path = System.IO.Path.GetFullPath(op.FileName);
+                labelaPotvrdaVreme.Content = fileName;
+            }
+        }
+
+        private void ButtonPotrosnja_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a file";
+            op.Filter = "All files (*.*)|*.*";
+
+            if (op.ShowDialog() == true)
+            {
+                string fileName = op.FileName;
+                //string path = System.IO.Path.GetFullPath(op.FileName);
+                labelaPotvrdaPotrosnja.Content = op.FileName;
+
+            }
+        }
+    }
+}
---
+++ EvidencijaPotrosnjeIVremenskihParametara/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Server;
+using Server.Klase;
+
+namespace EvidencijaPotrosnjeIVremenskihParametara
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static BindingList<TabelaPodaci> podaci { get; set; }
+
+        public MainWindow()
+        {
+            if(podaci == null)
+            {
+                podaci = new BindingList<TabelaPodaci>();
+            }
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public bool Validacija()
+        {
+            bool rez = true;
+
+            if (textBoxNaziv.Text == "")
+            {
+                rez = false;
+                textBoxNaziv.BorderBrush = Brushes.Red;
+                labelaGreskaNazivDrzaveFilter.Content = "Naziv drzave nije popunjen!";
+            }
+            else
+            {
+                textBoxNaziv.BorderBrush = Brushes.Green;
+                labelaGreskaNazivDrzaveFilter.Content = "";
+            }
+
+            if (textBoxSifra.Text == "")
+            {
+                rez = false;
+                textBoxSifra.BorderBrush = Brushes.Red;
+                labelaGreskaSifraDrzaveFilter.Content = "Sifra drzave nije popunjena!";
+            }
+            else
+            {
+                textBoxSifra.BorderBrush = Brushes.Green;
+                labelaGreskaSifraDrzaveFilter.Content = "";
+            }
+
+            if (textBoxMernoMesto.Text == "")
+            {
+                rez = false;
+                textBoxMernoMesto.BorderBrush = Brushes.Red;
+                labelaGreskaMernoMestoFilter.Content = "Merno mesto nije popunjeno!";
+            }
+            else
+            {
+                textBoxMernoMesto.BorderBrush = Brushes.Green;
+                labelaGreskaMernoMestoFilter.Content = "";
+            }
+
+            if (datePocetniFilter.SelectedDate == null)
+            {
+                rez = false;
+                datePocetniFilter.BorderBrush = Brushes.Red;
+                labelaGreskaDatumFilter.Content = "Datum nije izabran!";
+            }
+            else
+            {
+                datePocetniFilter.BorderBrush = Brushes.Green;
+                labelaGreskaDatumFilter.Content = "";
+            }
+
+            if (dateKrajnjiFilter.SelectedDate == null)
+            {
+                rez = false;
+                dateKrajnjiFilter.BorderBrush = Brushes.Red;
+                labelaGreskaDatumFilter.Content = "Datum nije izabran!";
+            }
+            else
+            {
+                dateKrajnjiFilter.BorderBrush = Brushes.Green;
+                labelaGreskaDatumFilter.Content = "";
+            }
+
+            return rez;
+        }
+
+        //private void ComboBoxDrzave()
+        //{
+        //    Drzava.drzave = new List<Drzava>();
+        //    Drzava.drzave.Add(new Drzava("Srbija", "SE", "Beograd"));
+        //    Drzava.drzave.Add(new Drzava("Albanija", "AL", "Tirana"));
+        //    Drzava.drzave.Add(new Drzava("Nemacka", "DE", "Berlin"));
+        //    Drzava.drzave.Add(new Drzava("Italija", "IT", "Rim"));
+        //    Drzava.drzave.Add(new Drzava("Litvanija", "LT", "Vilnus"));
+        //    Drzava.drzave.Add(new Drzava("Poljska", "PL", "Lodj"));
+        //    Drzava.drzave.Add(new Drzava("Norveska", "NO", "Oslo"));
+        //    Drzava.drzave.Add(new Drzava("Hrvatska", "HR", "Zagreb"));
+        //    Drzava.drzave.Add(new Drzava("Holandija", "NL", "Amsterdam"));
+        //    Drzava.drzave.Add(new Drzava("Rumunija", "RO", "Temisvar"));
+        //    List<string> nazivi = new List<string>();
+        //    foreach(var item in Drzava.drzave)
+        //    {
+        //        nazivi.Add(item.NazivDrzave);
+        //    }
+        //    comboBoxDrzaveFilter.ItemsSource = nazivi;
+        //}
+
+        private void ImportButton(object sender, RoutedEventArgs e)
+        {
+            ImportWindow importWindow = new ImportWindow();
+            importWindow.ShowDialog();
+
+        }
+
+
+        private void ButtonFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (Validacija())
+            {
+                int brojac = 0;
+                List<TabelaPodaci> pom = Data.UcitavanjeTabele();
+                foreach (var item in pom)
+                {
+                    Drzava pomDrzava = new Drzava(textBoxNaziv.ToString(), textBoxSifra.ToString(), textBoxMernoMesto.ToString());
+
+                    if (Drzava.drzave.Contains(pomDrzava))
+                    {
+                        if (pomDrzava.NazivDrzave == item.NazivDrzave && item.UctVreme >= datePocetniFilter.SelectedDate && item.UctVreme <= dateKrajnjiFilter.SelectedDate)
+                        {
+                            brojac++;
+                            continue;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                podaci.RemoveAt(brojac);
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                }
+                tabela.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Nisu dobro uneti podaci!", "Greska!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+        private void ButtonExport_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string path = "C:\\Users\\bojan\\Desktop\\6. semestar\\Razvoj softvera\\Projekat\\Projekat_RES\\Server\\export\\export.csv";
+            string csv = "";
+
+            List<TabelaPodaci> pomLista = Data.UcitavanjeTabele();
+
+            foreach(var item in pomLista)
+            {
+                if(csv != "")
+                {
+                    csv = csv + "\n" + String.Format($"{item.NazivDrzave},{item.SifraDrzave},{item.MernoMesto},{item.UctVreme},{item.KolicinaEnergije},{item.Temperatura},{item.AtmPritisak},{item.VlazVazduha},{item.BrzinaVetra}");
+                }
+                else
+                {
+                    csv = String.Format($"{item.NazivDrzave},{item.SifraDrzave},{item.MernoMesto},{item.UctVreme},{item.KolicinaEnergije},{item.Temperatura},{item.AtmPritisak},{item.VlazVazduha},{item.BrzinaVetra}");
+                }
+            }
+
+            File.WriteAllText(path, csv);
+        }
+    }
+}
